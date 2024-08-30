3dd90f02d22ff9cb987fd1c2224fedee9c8704bf
Abdul Hamid <ahamid9044@gmail.com>
2024-08-30 20:49:59 +0530

    Using the redux toolkit

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -6,24 +6,27 @@ const initialState={
     loading:false,
 };
 
-const  userSlice =createSlice({
+const userSlice=createSlice({
     name:'user',
     initialState,
     reducers:{
-        signInStart:(start)=>{
-            state.loading=true;
+        signInStart: (state)=>{
+            state.loading =true;
         },
         signInSuccess:(state,action)=>{
             state.currentUser=action.payload;
             state.loading=false;
-            state.error=null;
+            state.error =null;
         },
         signInFailure:(state,action)=>{
-            state.error=action.payload;
+            state.error =action.payload;
             state.loading =false;
         }
+
     }
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+
+export const { signInStart , signInSuccess, signInFailure}= userSlice.actions;
+
 export default userSlice.reducer;
