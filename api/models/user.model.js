f45a644376e35d190b3d1ab3f7f3decc861d39a2
Abdul Hamid <ahamid9044@gmail.com>
2024-08-31 18:22:41 +0530

    complete profile page UI

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
         },
     avatar:{
         type:String,
-        default:"https://in.images.search.yahoo.com/images/view;_ylt=AwrKAQqqtdJmeYIu.4i9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzkzM2RlNWYyYzRhOThiZWQzZTk2MjE4NTQ3NTJkOWE4BGdwb3MDNDYEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dnormal%2Bprofile%2Bpic%26ei%3DUTF-8%26type%3DE210IN885G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D46&w=2048&h=2048&imgurl=upload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2c%2FDefault_pfp.svg%2F2048px-Default_pfp.svg.png&rurl=https%3A%2F%2Fmungfali.com%2Fexplore%2FInstagram-Normal-PFP&size=62.8KB&p=normal+profile+pic&oid=933de5f2c4a98bed3e9621854752d9a8&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=Instagram+Normal+PFP&b=0&ni=80&no=46&ts=&tab=organic&sigr=4LGdxgW_IxGW&sigb=LHtN_fOplPEe&sigi=gtMfKOefKLp_&sigt=Qad_iFSEv2sw&.crumb=dIEVqtbZtpg&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E210IN885G0"
+        default:"https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg"
     },
 },{timestamps:true });
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "abdul-hamid-9c784.firebaseapp.com",
-  projectId: "abdul-hamid-9c784",
-  storageBucket: "abdul-hamid-9c784.appspot.com",
-  messagingSenderId: "908623593807",
-  appId: "1:908623593807:web:b01f37d1a38ddb4b2b453a",
-  measurementId: "G-7LX46L87W3"
+  authDomain: "mern-estate-1acbc.firebaseapp.com",
+  projectId: "mern-estate-1acbc",
+  storageBucket: "mern-estate-1acbc.appspot.com",
+  messagingSenderId: "381847786281",
+  appId: "1:381847786281:web:3923f6efbac5f78df75d38"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
