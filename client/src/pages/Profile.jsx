7307bc0bce2dfd6cf3a74d85b8b50293701c3eb5
Abdul Hamid <ahamid9044@gmail.com>
2024-09-12 10:59:08 +0530

    some extra changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,7 +141,7 @@ export default function Profile() {
           onChange={handleChange} />
         <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
-        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-centre hover:opacity-95' to={"/create-listing"} >Create Listing</Link>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"} >Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer' >Delete account</span>
