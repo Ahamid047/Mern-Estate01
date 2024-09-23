4b0f00e35d350b654f4a68442bc7674e113f6a32
Abdul Hamid <ahamid9044@gmail.com>
2024-09-23 21:38:29 +0530

    Fix vulnerabilities with npm audit fix

--- api/index.js
+++ api/index.js
@@ -10,9 +10,7 @@ dotenv.config();
 
 
 const _dirname=path.resolve(); 
-const app=express();
-app.use(express.json());
-app.use(cookieParser());
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -22,6 +20,10 @@ mongoose
     console.log(err);
   });
 
+  const app=express();
+
+  app.use(express.json());
+  app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("server is running on the port 3000!!");
