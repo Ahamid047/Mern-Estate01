d730e9a774706e045c8beb0ece3475bb85cf768c
Abdul Hamid <ahamid9044@gmail.com>
2024-09-23 19:19:57 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-slate-800 text-3xl font-bold mb-4'>Welcome to AbdulEstate – Your Trusted Real Estate Partner
+      </h1>
+
+      <p className='mb-4 text-slate-900'>At AbdulEstate, we are dedicated to making your property search easy and efficient. Whether you're buying, selling, or renting, our platform offers a wide range of curated listings tailored to meet your needs, from family homes to luxury estates and commercial spaces.</p>
+
+
+      <p className='mb-4 text-slate-900'>Our experienced team is committed to providing personalized service with expert guidance at every step of the process. We believe in building long-lasting relationships with our clients through trust, transparency, and exceptional support, ensuring that your real estate journey is smooth and stress-free.</p>
+
+
+      <p className='mb-4 text-slate-900'>With AbdulEstate, you’re not just choosing a property—you’re selecting a place to thrive. Explore our diverse portfolio and let us help you find the ideal space where you can create lasting memories and secure your future. Let’s build your dream today.</p>
+
+    </div>
   )
 }
