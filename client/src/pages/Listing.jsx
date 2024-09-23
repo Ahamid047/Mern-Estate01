06883347ee8c51d61e81c1d49c87045d52065d1c
Abdul Hamid <ahamid9044@gmail.com>
2024-09-23 20:04:50 +0530

    Fix vulnerabilities with npm audit fix

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, } from 'react';
 import { useParams } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'Swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
