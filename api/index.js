51ae1576e199eceffbe8b4061c48402b7fd971a0
Abdul Hamid <ahamid9044@gmail.com>
2024-09-23 19:33:54 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,10 +5,11 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser  from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 
-
+const _dirname=path.resolve(); 
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +32,12 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(_dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(_dirname,'client','dist', 'index.html'));
+  }
+)
 app.use((err, req, res, next) => {
   const statusCode = err.StatusCode || 500;
   const message =err.message|| 'Internal Server Error';
